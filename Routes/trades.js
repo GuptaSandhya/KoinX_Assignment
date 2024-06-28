03ec6296b2708e9cc789cd60e32c877c6d651fa8
sandhya <tech.eve27@gmail.com>
2024-06-28 14:29:27 +0530

    modification done

---
+++ Routes/trades.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const tradeController = require('../Controllers/tradeController');
+const multer = require('multer'); 
+const upload = multer({ dest: 'uploads/' }); 
+
+router.post('/upload', upload.single('file'), tradeController.uploadCsvFile);
+router.post('/balance', tradeController.getAssetBalance);
+
+module.exports = router;
+
